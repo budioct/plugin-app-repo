f8cbbb0ebb3c36b6f80972d87c84b20d9206ee2d
Budhi Octaviansyah <budioct@gmail.com>
2023-11-27 17:50:53 +0700

    add handler reqs/resp rest

---
+++ src/main/java/imam/corp/rest/handler/RestResponse.java
@@ -0,0 +1,54 @@
+package imam.corp.rest.handler;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+public class RestResponse {
+
+    @Getter
+    @Setter
+    @Data
+    @Builder
+    public static class list<T>{
+        private T list;
+        private Integer status_code;
+        private String message;
+        private String errors;
+        private restPagingResponse paging;
+    }
+
+    @Getter
+    @Setter
+    @Data
+    @Builder
+    public static class object<T> {
+        private T data;
+        private Integer status_code;
+        private String message;
+        private String errors;
+    }
+
+    @Getter
+    @Setter
+    @Data
+    @Builder
+    public static class restError<T> {
+        private Integer status_code;
+        private String message;
+        private String errors;
+    }
+
+    @Getter
+    @Setter
+    @Data
+    @Builder
+    public static class restPagingResponse {
+        private Integer currentPage;
+        private Integer totalPage;
+        private Integer sizePage;
+
+    }
+
+}

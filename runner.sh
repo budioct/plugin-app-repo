7a08a8bfb8250cbb0a0e818ae21c6667038f0c77
Budhi Octaviansyah <budioct@gmail.com>
2023-12-06 22:55:14 +0700

    update runner

--- runner.sh
+++ runner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar /home/budi/plugin-app-repo/target/springboot-mysql-docker.jar
+java -jar -Xmx32m -Xss256k -XX:MaxRAM=150m -XX:MaxRAMPercentage=70 -XX:+UseSerialGC /home/budi/plugin-app-repo/target/springboot-mysql-docker.jar

a5cfc3dd710652f8b6f38d0d0c7be709256fd83b
Budhi Octaviansyah <budioct@gmail.com>
2023-12-07 11:13:46 +0700

    update solve

--- Database.sql
+++ Database.sql
@@ -2,4 +2,102 @@ show databases;
 
 create database plugin_app;
 
+create database db_ttd_barang;
+use db_ttd_barang;
+
+show tables;
+
+
 use plugin_app;
+
+create table items
+(
+    id                   int(11) not null auto_increment,
+    nama_barang          varchar(255),
+    tanggal              timestamp,
+    no_npk               varchar(255),
+    bengkel_toko         varchar(200),
+    selesai_belumselesai boolean,
+    posisi_barang        varchar(255),
+    keterangan           text,
+    kapal                varchar(200),
+    create_at            timestamp,
+    update_modified_at   timestamp,
+    primary key (id)
+) engine = InnoDB;
+
+# table 1
+create table barang_terimas
+(
+    id                 int(11) not null auto_increment,
+--     no                 int(11),
+    tanggal            timestamp,
+    no_npp             varchar(200),
+    nama_barang        varchar(200),
+    keterangan         text,
+    kapal              varchar(200),
+    create_at          timestamp,
+    update_modified_at timestamp,
+    delete_at          int(1),
+    primary key (id)
+) engine = InnoDB;
+
+# table 2
+create table barang_perbaikans
+(
+    id                 int(11) not null auto_increment,
+--     no                 int(11),
+    tanggal            timestamp,
+    no_npk             varchar(200),
+    nama_barang        varchar(200),
+    keterangan         text,
+    kapal              varchar(200),
+    create_at          timestamp,
+    update_modified_at timestamp,
+    delete_at          int(1),
+    primary key (id)
+) engine = InnoDB;
+
+# table 3
+create table barang_perbaikan_admins
+(
+    id                   int(11) not null auto_increment,
+--     no                   int(11),
+    nama_barang          varchar(200),
+    tanggal              timestamp,
+    bengkel_toko         varchar(200),
+    selesai_belumselesai boolean,
+    posisi_barang        varchar(200),
+    keterangan           text,
+    kapal                varchar(200),
+    create_at            timestamp,
+    update_modified_at   timestamp,
+    delete_at            int(1),
+    primary key (id)
+) engine = InnoDB;
+
+
+show tables;
+describe items;
+describe barang_terimas;
+describe barang_perbaikans;
+describe barang_perbaikan_admins;
+
+# select * from items;
+select * from barang_terimas;
+select * from barang_perbaikans;
+select * from barang_perbaikan_admins;
+
+# drop table items;
+# drop table barang_terimas;
+# drop table barang_perbaikans;
+# drop table barang_perbaikan_admins;
+
+
+SELECT IFNULL(MAX(CONVERT(no, SIGNED INTEGER)), 0) AS kode
+FROM barang_terimas;
+SELECT IFNULL(MAX(CONVERT(no, SIGNED INTEGER)), 0) AS kode
+FROM barang_perbaikans;
+SELECT IFNULL(MAX(CONVERT(no, SIGNED INTEGER)), 0) AS kode
+FROM barang_perbaikan_admins;
+
--- src/test/java/imam/corp/ORMTests.java
+++ src/test/java/imam/corp/ORMTests.java
@@ -1,17 +1,22 @@
 package imam.corp;
 
+import imam.corp.config.converter.StringToDateConverter;
 import imam.corp.modules.barangterimas.BarangTerimaEntity;
 import imam.corp.modules.barangterimas.BarangTerimaRepository;
 import imam.corp.modules.barangperbaikan.BarangPerbaikanRepository;
 import imam.corp.modules.bperbaikanadmin.BPerbaikanAdminEntity;
 import imam.corp.modules.bperbaikanadmin.BPerbaikanAdminRepository;
+import imam.corp.utilities.SecuritySecretKey;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.lang.reflect.Field;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @SpringBootTest
@@ -25,6 +30,12 @@ public class ORMTests {
 
     @Autowired
     BPerbaikanAdminRepository bPerbaikanAdminRepository;
+
+    @Autowired
+    SecuritySecretKey secretKey;
+
+    @Autowired
+    StringToDateConverter converter;
 
     @Test
     void testInsertData() {
@@ -40,9 +51,6 @@ public class ORMTests {
         String text = "Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the li Where does it come from? Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.";
 
     }
-
-
-
 
 
     @Test
@@ -62,7 +70,6 @@ public class ORMTests {
     void testBPerbaikanAdmin() {
 
         BPerbaikanAdminEntity bpadmin = new BPerbaikanAdminEntity();
-        bpadmin.setNo(111L);
         bpadmin.setNamaBarang("nama barang");
         bpadmin.setTanggal(LocalDateTime.now());
         bpadmin.setBengkelToko("bengkel");
@@ -76,8 +83,64 @@ public class ORMTests {
     }
 
     @Test
-    void gc(){
+    void gc() {
         System.gc();
+    }
+
+    @Test
+    void testKey() {
+
+        String sebenarnya = secretKey.secretKey();
+        String ekpetasi = "b{WV92TI-1DdKN<+B<pV*1D*^A_gi>|?o{C`aP7i4].?Y_4-w<Dy@5%4)]gJdi";
+
+        Assertions.assertNotNull(sebenarnya);
+        Assertions.assertEquals(ekpetasi, sebenarnya);
+
+    }
+
+    @Test
+    void whenTrim_thenCorrect() {
+
+        String data = "     Hello World!!!     ";
+        log.info("Original:  {}", data.length());
+        log.info("length Original:  {}", data);
+        log.info("modifed trim:  {}", data.trim());
+        log.info("length modifed trim:  {}", data.trim().length());
+
+    }
+
+    @Test
+    void testStringConvertToLocalDateTime() {
+
+        DateTimeFormatter DATEFORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String ld = "23-12-2023";
+//        LocalDate ld = LocalDate.parse("03-12-2023", DATEFORMATTER);
+        LocalDateTime ldt = LocalDateTime.of(converter.convert(ld), LocalDateTime.now().toLocalTime());
+        System.out.println(ld);
+        System.out.println(ldt);
+
+    }
+
+    @Test
+    void testLocalDateConvertToString(){
+
+        BarangTerimaEntity barangTerima = barangTerimaRepository.findFirstById(59L)
+                .orElseThrow(() -> new RuntimeException("Id not found"));
+//        LocalDate localDate = barangTerima.getTanggal().toLocalDate();
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+//        String text = localDate.format(formatter);
+//        LocalDate parseDate = LocalDate.parse(text, formatter);
+
+//        log.info("{}", barangTerima.getTanggal()); // 2023-04-27T01:20:13
+//        log.info("{}", localDate); // 2023-04-27
+//        log.info("{}", text); // 27/04/2023
+//        log.info("{}", parseDate); // 2023-04-27
+
+        String convert = converter.convert(barangTerima.getTanggal());
+
+        log.info("{}", convert);
+
+
     }
 
 
